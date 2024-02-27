9f5cb09373a9092ea755159f7dd74a153d165afd
Ivan Stasevich <pravitel2015ify@gmail.com>
2024-02-27 19:22:49 +0300

    Fix tests

--- src/number_theory/__init__.py
+++ src/number_theory/__init__.py
@@ -1,4 +1,5 @@
 from .divisors import divisors_naive, divisors
+from .gcd import gcd_naive, gcd_euclid, gcd_euclid_rec
 from .is_prime import is_prime_naive, is_prime
 from .primality_tests import decompose, primality_tests
 from .rsa import generate_keypair, encrypt
@@ -14,4 +15,7 @@ __all__ = [
     "sieve",
     "primality_tests",
     "decompose",
+    "gcd_naive",
+    "gcd_euclid",
+    "gcd_euclid_rec",
 ]
