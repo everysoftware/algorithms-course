58a1d09412251467618a78795a97fcaa1f8de125
ivan.stasevich <pravitel2015ify@gmail.com>
2026-04-17 19:12:34 +0300

    feat: implement journey path algorithm

---
+++ tests/test_graphs/test_journey_path.py
@@ -0,0 +1,27 @@
+from collections.abc import Callable
+from typing import Any
+
+import pytest
+
+from src.n_graphs.journey_path import Flight, get_route, get_route_enhanced
+
+
+@pytest.mark.parametrize("func", [get_route, get_route_enhanced])
+@pytest.mark.parametrize(
+    ("flights", "expected"),
+    [
+        ([("Москва", "Белград")], ["Москва", "Белград"]),
+        ([("Москва", "Белград"), ("Москва", "Ереван")], ["Ереван", "Москва", "Белград"]),
+        (
+            [("Ереван", "Москва"), ("Москва", "СПБ"), ("СПБ", "Белград")],
+            [
+                "Ереван",
+                "Москва",
+                "СПБ",
+                "Белград",
+            ],
+        ),
+    ],
+)
+def test_journey_path(func: Callable[..., Any], flights: list[Flight], expected: list[str]) -> None:
+    assert func(flights) == expected or func(flights) == list(reversed(expected))
